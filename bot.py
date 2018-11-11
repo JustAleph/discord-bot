9d792c74915a1186d8e60fbaeb5b421c57e07f2a
JustAleph <44878908+JustAleph@users.noreply.github.com>
2018-11-11 14:09:50 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ client = commands.Bot(command_prefix = ".")
 
 @client.event
 async def on_ready():
-    await client.change_presence(game=discord.Game(name='Alt Gen | +help | v0,1'))
+    await client.change_presence(game=discord.Game(name='Alt Gen | .help | v0,1'))
 
     print('Logged in as')
     print(client.user.name)
