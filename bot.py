88bd226c9c98fa7509d526657029afccb886e6db
JustAleph <44878908+JustAleph@users.noreply.github.com>
2018-11-11 12:21:34 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,7 @@
 import discord
-from discord.ext.commands import Bot
 from discord.ext import commands
-import asyncio
-import random
-import time
 
-Client = discord.Client()
-client = commands.Bot(command_prefix = "+")
+client = commands.Bot(command_prefix = ".")
 
 @client.event
 async def on_ready():
@@ -19,15 +14,15 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content == "+minecraft":
+    if message.content == ".minecraft":
         await client.send_message(message.author, random.choice([" **Alt Generator** \n Here is your Minecraft Alt! \n email:pass \n \n Made by BlockBuster594✓ᵛᵉʳᶦᶠᶦᵉᵈ#6784 ", " Alt Generator \n Here is your Minecraft Alt! \n email:pass \n \n Made by BlockBuster594✓ᵛᵉʳᶦᶠᶦᵉᵈ#6784 "]))
-    if message.content == "+special":
+    if message.content == ".special":
         await client.send_message(message.author, random.choice([" **Alt Generator** \n Here is your Special Alt! \n email:pass \n \n Made by BlockBuster594✓ᵛᵉʳᶦᶠᶦᵉᵈ#6784 ", " Alt Generator \n Here is your Special Alt! \n email:pass \n \n Made by BlockBuster594✓ᵛᵉʳᶦᶠᶦᵉᵈ#6784 "]))
-    if message.content == "+help":
-        await client.send_message(message.channel, "**Commands** \n \n+help \n Show this. \n \n+minecraft \n Only works in the generator channel. \n \n+special \n Only works in the generator channel. \n \n+shop \n Sends the shop link.")
-    if message.content == "+shop":
+    if message.content == ".help":
+        await client.send_message(message.channel, "**Commands** \n \n.help \n Show this. \n \n.minecraft \n Only works in the generator channel. \n \n.special \n Only works in the generator channel. \n \n.shop \n Sends the shop link.")
+    if message.content == ".shop":
         await client.send_message(message.channel, "**Shop**: \nhttps://selly.gg/@Refresh")
-    if message.content == "+invite":
+    if message.content == ".invite":
         await client.send_message(message.channel, "this is the bots invite link: ```https://discordapp.com/oauth2/authorize?&client_id=509798730942382080&scope=bot&permissions=8``` ")
 
 client.run("NTA5Nzk4NzMwOTQyMzgyMDgw.DshoYw.t2h6O_b66wCHm7Ij7P-unWy6nOg")
