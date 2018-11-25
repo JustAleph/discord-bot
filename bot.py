24492aaad93b3e5a6c6831aff3aa5cf20577b66b
JustAleph <44878908+JustAleph@users.noreply.github.com>
2018-11-25 17:41:51 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,4 +39,4 @@ async def on_message(message):
         embed.set_footer(text="Refresh Alts © 2018 all rights reserved",icon_url= "https://cdn.discordapp.com/avatars/509798730942382080/9b2a0235965da5f7b06503fbbddad025.png?size=512")
         await client.send_message(message.channel, embed=embed)
 
-client.run("NTA5Nzk4NzMwOTQyMzgyMDgw.DtwG7A.uX907ZYeCwDuaFZEASVi-RabJTw")
+client.run("process.env.TOKEN")
